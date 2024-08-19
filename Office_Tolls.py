f215483a6ec98dab70759dba0f5111db8de9d25d
swapnil kumar <swapnilk0294@gmail.com>
2024-08-19 10:31:07 +0200

    chore: Update logo URL in Office_Tolls.py to use "experline.png"

--- Office_Tolls.py
+++ Office_Tolls.py
@@ -10,14 +10,10 @@ SECRET_PHRASE = st.secrets["secret_phrase"]
 client = MongoClient(MONGO_URI)
 db = client["aino_db"]
 users_collection = db["users"]
-LOGO_URL_SMALL = "aino.png"
+LOGO_URL_SMALL = "experline.png"
 st.set_page_config(page_title="ExpertLine", page_icon="🧠", layout="wide")
-st.logo(image="aino.png",icon_image=LOGO_URL_SMALL)
+st.logo(image="experline.png",icon_image=LOGO_URL_SMALL)    
 # Function to hash passwords
-
-
-
-
 
 def show_tutorials():
     st.write(    """
